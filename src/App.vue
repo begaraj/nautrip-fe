7b3fa5da77e5c5665233e7da85319ab8048f2506
Robert <robert@Roberts-MacBook-Pro.local>
2017-03-19 07:14:52 +0100

    add top bar

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,23 @@
 <template>
   <div id="app">
-    <router-view></router-view>
+    <Menu mode="horizontal" :theme="dark" active-key="1">
+      
+      <router-link to="/create-post">
+        <Menu-item key="1">
+          <Icon type="plus-round"></Icon>
+          Create new experience
+        </Menu-item>
+      </router-link>
+      <router-link to="/profile">
+        <Menu-item key="2">
+          <Icon size="20" type="person"></Icon>
+          My Profile
+        </Menu-item>
+      </router-link>
+    </Menu>
+
+    <router-view class="router-view"></router-view>
+
     <Menu mode="horizontal" :theme="theme1" active-key="1" style="width:100%; text-align:center; display:block; position:relative; bottom:0;">
         <Menu-item key="1" name="menuItem" >
             <Icon size="20" type="android-more-vertical"></Icon>
@@ -16,10 +33,6 @@
         <Menu-item key="4" name="menuItem">
             <Icon size="20" type="map"></Icon>
             Nearby
-        </Menu-item>
-        <Menu-item key="5" name="menuItem">
-            <Icon size="20" type="person"></Icon>
-          <router-link to="/profile">My Profile</router-link>
         </Menu-item>
     </Menu>
   </div>
@@ -44,10 +57,13 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 
 .ivu-menu-horizontal .ivu-menu-item, .ivu-menu-horizontal .ivu-menu-submenu{
   float:none!important; display: inline-block;
 }
+
+  .router-view {
+    margin-top: 2em;
+  }
 </style>
